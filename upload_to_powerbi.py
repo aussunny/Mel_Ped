d665aa8ce2c246a8e7b733fb3b63d6d9bd2ba62f
Sunny Miao Sun <aussunnysun@gmail.com>
2021-10-07 10:04:54 +1100

    Update upload_to_powerbi.py

--- upload_to_powerbi.py
+++ upload_to_powerbi.py
@@ -1,5 +1,7 @@
 import boto3
 import awswrangler as wr
+# configure AWS CLI to use boto3 with your own bucket
 s3 = boto3.resource('s3')
+# change to download the monthly count file data = wr.s3.read_csv('s3://mel-ped-count/test/month_ped_count.csv')
 data = wr.s3.read_csv('s3://mel-ped-count/test/day_ped_count.csv') 
 print(data)
